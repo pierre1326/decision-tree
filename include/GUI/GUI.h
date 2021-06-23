f5b1fc0d4a73fafd5f34591f24fe9d38abd2c7f2
Jean Pierre <pierre1326@hotmail.com>
2021-06-23 10:20:01 -0600

    Documentacion GUI

--- include/GUI/GUI.h
+++ include/GUI/GUI.h
@@ -15,18 +15,48 @@ using namespace std;
 class Gui {
   
   public:
+    /**
+     * Constructor de la consola principal
+     * 
+     * @param path  Ruta del arbol a cargar
+     **/
     Gui(string path);
+
+    /**
+     * Inicia la interfaz por consola 
+     **/
     void init();
 
   private:
+    //Arbol con la informacion del chatbot
     Tree tree;
+    //Bot usado para la conversacion
     Bot bot;
+    //Archivo con la informacion del arbol
     File file;
+    //Ruta del archivo
     string path;
 
+    /**
+     * Ejecuta la opcion indicado por el usuario
+     * 
+     * @param option  Opcion ingresada por el usuario
+     **/
     void executeOption(int option);
+
+    /**
+     * Solicita la informacion e ingresa el nodo al arbol
+     **/
     void addNode();
+
+    /**
+     * Elimina el nodo del arbol con el ID dado por el usuario
+     **/
     void deleteNode();
+
+    /**
+     * Inicia la conversacion con el chatbot
+     **/
     void initBot();
 
 };
